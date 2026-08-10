6805dd08cc8c2d85a1f5a9c25677b6885e38fb8c
GoodLoongStudio <chnmlong@hotmail.com>
2026-08-10 12:50:29 +0800

    make RTS edge scrolling usable

--- source/match/IsometricCamera3D.gd
+++ source/match/IsometricCamera3D.gd
@@ -7,7 +7,7 @@ const EXPECTED_PROJECTION = PROJECTION_ORTHOGONAL
 @export var size_min = 1
 @export var size_max = 20
 @export_group("Movement")
-@export var screen_margin_for_movement = 2  # px
+@export var screen_margin_for_movement = 32  # px
 @export var movement_speed = 1.1
 @export var bounding_planes: Array[Plane] = []
 @export_group("Rotation")
