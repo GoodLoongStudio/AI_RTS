bbf154cb33c9e975ff3c3328e958d4472acf00cc
GoodLoongStudio <chnmlong@hotmail.com>
2026-08-10 13:29:42 +0800

    增加可保存的 RTS 镜头设置参数

--- source/data-model/Options.gd
+++ source/data-model/Options.gd
@@ -7,6 +7,16 @@ enum Screen { FULL = 0, WINDOW = 1 }
 @export var mouse_restricted = false:
 	set = _set_mouse_restricted
 
+# RTS camera preferences. These stay in the same user:// options resource as the
+# existing display settings, so old save files simply receive the defaults below.
+@export_category("Camera")
+@export var camera_edge_scroll_enabled := true
+@export_range(0.4, 3.0, 0.1) var camera_movement_speed := 1.1
+@export_range(16.0, 96.0, 1.0) var camera_edge_margin := 48.0
+@export_range(24.0, 128.0, 1.0) var camera_bottom_edge_margin := 72.0
+@export_range(3.0, 24.0, 1.0) var camera_smoothing := 10.0
+@export_range(0.25, 3.0, 0.25) var camera_zoom_step := 1.0
+
 
 func _init():
 	_apply_stored_options()
@@ -42,3 +52,12 @@ func _apply_mouse_restricted():
 		Input.set_mouse_mode(Input.MOUSE_MODE_CONFINED)
 	else:
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
+
+func reset_camera_to_defaults():
+	camera_edge_scroll_enabled = true
+	camera_movement_speed = 1.1
+	camera_edge_margin = 48.0
+	camera_bottom_edge_margin = 72.0
+	camera_smoothing = 10.0
+	camera_zoom_step = 1.0
